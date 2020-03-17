05a1ba8415c35d209d03caa1d5cb4185f9e0260c
kcl <1176382218@qq.com>
2020-03-17 17:22:35 +0800

    add sql ddl

---
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package life.majiang.community.mapper;
+
+/**
+ * 项目名： community
+ * 包名:    life.majiang.community.mapper
+ * 文件名   UserMapper
+ * 创建者
+ * 创建时间: 2020/3/16 12:38 AM
+ * 描述  ${TODO}
+ */
+public class UserMapper {
+}
+
---
+++ src/main/java/life/majiang/community/model/User.java
@@ -0,0 +1,13 @@
+package life.majiang.community.model;
+
+/**
+ * 项目名： community
+ * 包名:    life.majiang.community.model
+ * 文件名   User
+ * 创建者
+ * 创建时间: 2020/3/16 12:40 AM
+ * 描述  ${TODO}
+ */
+public class User {
+}
+
